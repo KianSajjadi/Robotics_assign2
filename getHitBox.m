99d0109a0f3d62a3c0f5782697f3d8c3717bd2f5
kiansajjadi <kian.s.sajjadi@gmail.com>
2020-10-09 20:17:52 +1100

    initialising getHitBox function

---
+++ getHitBox.m
@@ -0,0 +1,14 @@
+%% hitBox = getHitBox(firstPointTransform, secondPointTransform, width, 
+%Arguments:
+%   firstPointTransform: a 4x4 homogenous transform of the first point
+%   secondPointTransform: a 4x4 homogenous transform of the second point
+%   width:  the width of the box (x axis)
+%   length: the length of the box (y axis)
+%Output:
+%   hitBox: a cellArray 
+%Description:
+%
+
+function hitBox = getHixBox(firstPointTransform, secondPointTransform, width, length)
+
+end
